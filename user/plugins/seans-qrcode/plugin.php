1292379efd505f15bb9026b4b240ca93b757d1e5
Chris Gilligan <49878588+UTCGilligan@users.noreply.github.com>
2025-07-21 09:41:22 -0400

    Revert "UTCT-112: Update PHP dependency and fix deprecated code."
    This reverts commit e5242d48d8c96a0e6338bd020f73d57473c56a47.

--- user/plugins/seans-qrcode/plugin.php
+++ user/plugins/seans-qrcode/plugin.php
@@ -37,9 +37,8 @@ use chillerlan\QRCode\{QRCode, QROptions};
 
 class LogoOptions extends QROptions{
 	// size in QR modules, multiply with QROptions::$scale for pixel size
-	protected ?int $logoSpaceWidth = null; // or some integer value
-	protected ?int $logoSpaceHeight = null; // Make this nullable to match parent class
-
+	protected int $logoSpaceWidth;
+	protected int $logoSpaceHeight;
 }
 
 // Kick in if the loader does not recognize a valid pattern
